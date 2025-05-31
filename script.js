e9f039af0f50c7a0a7bea49ef9ab9923c1bc5688
VikrantAwate1046 <122825881+VikrantAwate1046@users.noreply.github.com>
2025-05-31 15:48:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("Thank you for your feedback!");
 });
+
+const images = document.querySelectorAll(".gallery img");
+const overlay = document.getElementById("overlay");
+const overlayImg = document.getElementById("overlay-img");
+const closeBtn = document.getElementById("close-btn");
+
+images.forEach((img) => {
+  img.addEventListener("click", () => {
+    overlay.style.display = "flex";
+    overlayImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
